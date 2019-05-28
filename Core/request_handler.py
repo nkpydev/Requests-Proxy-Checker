598dd5cbf9010bd53bf38db75d20d3bc8a691d72
Nitin K <39520442+nkpydev@users.noreply.github.com>
2019-05-28 21:07:47 +0530

    updated Comments

--- Core/request_handler.py
+++ Core/request_handler.py
@@ -1,8 +1,10 @@
 # ---- Requests Imports ---- #
 import requests
+# --- Project Internal Imports --- #
+# --- Exceptions --- #
 from requests.exceptions import ProxyError,HTTPError,TooManyRedirects,Timeout,SSLError,ConnectTimeout
 
-
+# --- Class ProxyChecker, which will have a 'ip_resolver()' function to do requests and get status_code or exception --- #
 class ProxyChecker():
     def __init__(self):
         self.target_url = ''
@@ -13,10 +15,11 @@ class ProxyChecker():
         #self.return_list = []
         #self.return_list.clear()
 
+    # --- Function to do requests and accordingly either get status_code[mostly SUCCESS] or exception [NOT WORKING]
     def ip_resolver(self,ip,port,target_url):
         self.headers = {'User-Agent': self.user_agent}
         self.proxies = {'http': 'http://{}:{}'.format(ip,port)}
-
+        
         try:
             pro = requests.get(target_url,timeout=self.time_out,proxies=self.proxies,headers=self.headers)
             return [ip,target_url,pro.status_code]
