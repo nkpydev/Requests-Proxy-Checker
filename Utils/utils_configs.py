37b4c0f99840687c7ab2fb7bd28b5cae46b57231
Nitin K <39520442+nkpydev@users.noreply.github.com>
2019-05-28 21:04:46 +0530

    Documentation with # Comments

--- Utils/utils_configs.py
+++ Utils/utils_configs.py
@@ -1,13 +1,13 @@
+# --- Generic Imports --- #
 import os
 from os.path import exists
 import csv
+# --- Project Internal Imports --- #
 from Core.exceptions import ProxyCheckerDirHandlingError, ProxyCheckerRequestException
 
 # ---- DIR Handling ---- #
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-PROJECT_PARENT_DIR = os.path.split(BASE_DIR)[0]
-DATA_DIR = os.path.join(PROJECT_PARENT_DIR,'Data')
-INPUT_FILE = os.path.join(DATA_DIR,'input.txt')
-#print(INPUT_FILE)
-OUTPUT_FILE = os.path.join(DATA_DIR,'output.csv')
-#print(OUTPUT_FILE)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__)) # BASE_DIR
+PROJECT_PARENT_DIR = os.path.split(BASE_DIR)[0] # PARENT_DIR OF BASE_DIR
+DATA_DIR = os.path.join(PROJECT_PARENT_DIR,'Data') # Data - Source of input.txt and output.csv
+INPUT_FILE = os.path.join(DATA_DIR,'input.txt') # Input file, store your IPs to test inside this file.
+OUTPUT_FILE = os.path.join(DATA_DIR,'output.csv') # Output file, the result will be stored into this.
