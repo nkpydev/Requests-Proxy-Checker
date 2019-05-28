4287ff80d2d86a160eded691679c66ac0c5d9c57
Nitin K <39520442+nkpydev@users.noreply.github.com>
2019-05-28 21:11:09 +0530

    Documentation done

--- proxy_checker.py
+++ proxy_checker.py
@@ -1,18 +1,26 @@
+# --- Generic Imports --- #
+import csv
+
+# --- Project Internal Imports --- #
 from Utils.utils_configs import INPUT_FILE,OUTPUT_FILE
 from Core.request_handler import ProxyChecker
 from Core.exceptions import ProxyCheckerDirHandlingError, ProxyCheckerRequestException
-import csv
 
+
+# --- Port Number --- #
 port = '2007' # Change Port to your need
 
+# --- Target URLs --- #
 # Change URLs as per your choice
 target_urls = ['http://google.com',
                 'http://github.com',
                 'http://msn.com'                
             ]
 
+# --- End Result Store into List --- #
 result = []
 
+# --- Open input.txt and read lines, process 1 at a time, store return into a list --- #
 with open(INPUT_FILE,'r') as rf:
     in_put = rf.readlines()
     for ip in in_put:
@@ -24,9 +32,8 @@ with open(INPUT_FILE,'r') as rf:
         for url in target_urls:
             check = ProxyChecker()
             result.append(check.ip_resolver(new_ip,port,url))
-
-# print(result)
-
+# --- Open output.csv and writerow, 1 at a time from the result [list] --- #
 with open(OUTPUT_FILE,'w',newline='') as of:
     writer = csv.writer(of)
     writer.writerows(result)
+print('DONE!!')
